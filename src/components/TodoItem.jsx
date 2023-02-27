8d9f95fbcb10c6b2a47768e0162dbd9510d56f1a
danyaITi <danil.lenskiy.1997@mail.ru>
2023-02-27 18:29:30 +0300

    add update todo request

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,27 @@
+import { useMutation } from '@apollo/client';
 import { Checkbox, Text, CloseButton, HStack } from '@chakra-ui/react';
+import { UPDATE_TODO } from '../apollo/todos';
 
-const TodoItem = ({ id, title }) => {
+const TodoItem = ({ id, title, completed }) => {
+  const [toggleTodo, { error }] = useMutation(UPDATE_TODO);
+
+  if (error) {
+    return <h2>Error...</h2>;
+  }
+
   return (
     <HStack spacing={3}>
-      <Checkbox />
+      <Checkbox
+        isChecked={completed}
+        onChange={() =>
+          toggleTodo({
+            variables: {
+              id: id,
+              completed: !completed,
+            },
+          })
+        }
+      />
       <Text>{title}</Text>
       <CloseButton />
     </HStack>
